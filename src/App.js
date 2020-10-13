46e398d4fb10d701f1908f9fda560060f4e6a099
Aymeric-Lancien <aymericlancien12@gmail.com>
2020-10-13 17:12:09 +0200

    new contacts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact name = "Micheline" online = {true} image = "https://randomuser.me/api/portraits/women/39.jpg"/>
+      <Contact name = "Ida" online = {false} image = "https://randomuser.me/api/portraits/women/5.jpg"/>
+      <Contact name = "Roger" online = {true} image = "https://randomuser.me/api/portraits/men/94.jpg"/>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact() {
+/*function Contact() {
   return (
     <div className="Contact">
       <img className="avatar" src="https://randomuser.me/api/portraits/men/50.jpg" alt=""/>
@@ -10,6 +10,21 @@ function Contact() {
         <div className="status">
           <span className="status-online"></span>
           <span className="status-text">Online</span>
+        </div>
+      </div>
+    </div>
+  );
+}*/
+
+function Contact(props) {
+  return (
+    <div className="Contact">
+      <img className="avatar" src={props.image} alt=""/>
+      <div>
+        <h4 className="name">{props.name}</h4>
+        <div className="status">
+          <span className={props.online ? "status-online" : "status-offline"}></span>
+          <span className="status-text">{props.online ? "Online" : "Offline"}</span>
         </div>
       </div>
     </div>
