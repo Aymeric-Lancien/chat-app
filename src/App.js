db603a439ca8164d8436fa325f5a68536142e494
Aymeric-Lancien <aymericlancien12@gmail.com>
2020-10-14 12:23:18 +0200

    contact list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact name = "Micheline" online = {true} image = "https://randomuser.me/api/portraits/women/39.jpg"/>
-      <Contact name = "Ida" online = {false} image = "https://randomuser.me/api/portraits/women/5.jpg"/>
-      <Contact name = "Roger" online = {true} image = "https://randomuser.me/api/portraits/men/94.jpg"/>
+      <ContactList />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import './Contact.css';
 
-/*function Contact() {
-  return (
-    <div className="Contact">
-      <img className="avatar" src="https://randomuser.me/api/portraits/men/50.jpg" alt=""/>
-      <div>
-        <h4 className="name">Rodney Lambert</h4>
-        <div className="status">
-          <span className="status-online"></span>
-          <span className="status-text">Online</span>
-        </div>
-      </div>
-    </div>
-  );
-}*/
+
 
 function Contact(props) {
   return (
     <div className="Contact">
-      <img className="avatar" src={props.image} alt=""/>
+      <img className="avatar" src={props.avatar} alt=""/>
       <div>
         <h4 className="name">{props.name}</h4>
         <div className="status">
